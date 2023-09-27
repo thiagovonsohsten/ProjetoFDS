5461d8c34a3b813418811ee1774b344255c2de69
Thiago <thiagolvs10@gmail.com>
2023-09-27 13:09:18 -0300

    alterações

--- Projeto_FDS/ProjetoFDS/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from loja.views import registro, nicho  # Importa as views do aplicativo 'loja'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('registro/', registro, name='registro'),  # Usa 'registro' importado de 'loja.views'
-    path('nicho/', nicho, name='nicho'),  # Usa 'nicho' importado de 'loja.views'
-]
