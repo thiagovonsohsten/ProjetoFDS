85cc5207dea6f142712a7f47bbf66ea6871b41ea
Thiago <thiagolvs10@gmail.com>
2023-09-27 08:32:52 -0300

    limpando

--- urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from app_praia import views
-
-urlpatterns = [
-# rota, view respossável, nome de refererência
-# lojadepraia.com
-path('',views.home,name='home'),
-]
